c14950cd5a86187a3411f9bf5df9ad0b5ac0bbfe
unknown <covarrubias.seb@gmail.com>
2023-10-01 16:33:29 -0700

    Added feature to also start the game on first keydown in the input answer element.

--- js/application.js
+++ js/application.js
@@ -1,3 +1,5 @@
+let gameStarted = false;
+
 function generateProblem() {
 
   let a = Math.floor(Math.random() * 99);
@@ -13,49 +15,107 @@ function displayProblem(values) {
 
 }
 
+function startGame() {
+
+  // Clear Score
+  $(".current-score").text(0);
+
+  // Generate First Math Problem
+  let values = generateProblem();  
+  let answer = values[0] + values[1];
+
+  // Display New Problem in DOM
+  displayProblem(values);
+
+  return answer;
+
+}
+
 $(document).ready( () => {
+
+  let answer;
 
   // Start Game
   $(".new-game-button").on("click", () => {
 
-    // Clear Score
-    $(".current-score").text(0);
+    gameStarted = true;
 
-    // Generate First Math Problem
-    let values = generateProblem();  
-    let answer = values[0] + values[1];
+    answer = startGame();
 
-    // Display New Problem in DOM
-    displayProblem(values);
+    // // On user input, check if it's correct
+    // // If it is correct, increment the current score and
+    // // generate a new math problem
+    // $(".math-answer-attempt").on("change", (event) => {
 
-    // On user input, check if it's correct
-    // If it is correct, increment the current score and
-    // generate a new math problem
-    $(".math-answer-attempt").on("change", (event) => {
+    //   if (!gameStarted) {
 
-      let answerAttempt = Number($(".math-answer-attempt").val());
+    //     gameStarted = true;
 
-      // Answer is correct
-      if (answerAttempt === answer) {
+    //     startGame();
 
-        let currentScore = $(".current-score").text();
 
-        // Increment and display score
-        currentScore ? 
-          $(".current-score").text(Number(currentScore) + 1) :
-          $(".current-score").text(0);
+    //   }
 
-        // Generate a new problem
-        values = generateProblem();
-        answer = values[0] + values[1];
+    //   let answerAttempt = Number($(".math-answer-attempt").val());
 
-        // Display New Problem in DOM
-        displayProblem(values);
+    //   // Answer is correct
+    //   if (answerAttempt === answer) {
 
-      }
+    //     let currentScore = $(".current-score").text();
 
-    });
+    //     // Increment and display score
+    //     currentScore ? 
+    //       $(".current-score").text(Number(currentScore) + 1) :
+    //       $(".current-score").text(0);
+
+    //     // Generate a new problem
+    //     values = generateProblem();
+    //     answer = values[0] + values[1];
+
+    //     // Display New Problem in DOM
+    //     displayProblem(values);
+
+    //   }
+
+    // });
 
   });
+
+  // On user input, check if it's correct
+  // If it is correct, increment the current score and
+  // generate a new math problem
+  $(".math-answer-attempt").on("keydown", (event) => {
+
+    if (!gameStarted) {
+
+      gameStarted = true;
+
+      answer = startGame();
+
+    }
+
+    let answerAttempt = Number($(".math-answer-attempt").val());
+
+    // Answer is correct
+    if (answerAttempt === answer) {
+
+      let currentScore = $(".current-score").text();
+
+      // Increment and display score
+      currentScore ? 
+        $(".current-score").text(Number(currentScore) + 1) :
+        $(".current-score").text(0);
+
+      // Generate a new problem
+      values = generateProblem();
+      answer = values[0] + values[1];
+
+      // Display New Problem in DOM
+      displayProblem(values);
+
+    }
+
+  });
+
 
 });
